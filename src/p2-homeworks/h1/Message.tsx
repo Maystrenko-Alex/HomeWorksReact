4d490e4165469a3cd4ec26844c74733c7aa126f4
AlexMaystrenko <maystrenko_alex@mail.ru>
2021-12-27 21:09:06 +0300

    add hw1

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import s from './Message.module.css';
+
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+export function Message(props: MessagePropsType) {
+    return (
+        <div className={s.message}>
+            <img className={s.messageAva} src={props.avatar} alt={'ava img'}/>
+            <div className={s.messageBlockCorner}></div>
+            <div className={s.messageBlock}>
+                <div className={s.messageBlockName}>{props.name}</div>
+                <div className={s.messageBlockText}>{props.message}</div>
+                <div className={s.messageBlockTime}>{props.time}</div>
+            </div>
+        </div>
+    );
+}
+
+export default Message
